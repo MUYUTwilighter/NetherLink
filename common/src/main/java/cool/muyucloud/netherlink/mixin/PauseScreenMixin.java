06ec14ef7cdd6f2b7cd2019b39d60d996cee38b6
MUYU_Twilighter <1484605372@qq.com>
2026-05-22 23:02:52 +0800

    optimize ui positioning

--- common/src/main/java/cool/muyucloud/netherlink/mixin/PauseScreenMixin.java
+++ common/src/main/java/cool/muyucloud/netherlink/mixin/PauseScreenMixin.java
@@ -3,11 +3,14 @@ package cool.muyucloud.netherlink.mixin;
 import cool.muyucloud.netherlink.client.ClientP2PController;
 import cool.muyucloud.netherlink.client.NetherLinkFriendsScreen;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,23 +24,30 @@ public abstract class PauseScreenMixin extends Screen {
     @Inject(method = "createPauseMenu", at = @At("TAIL"))
     private void onCreatePauseMenu(CallbackInfo ci) {
         IntegratedServer server = this.minecraft.getSingleplayerServer();
-        int y = this.height / 4 + 152;
-        if (server != null && server.isPublished()) {
-            this.addRenderableWidget(Button.builder(label(server), button -> {
-                boolean open = !ClientP2PController.isFriendsOpen(server);
-                ClientP2PController.setFriendsOpen(this.minecraft, server, open);
-                button.setMessage(label(server));
-            }).bounds(this.width / 2 - 102, y, 204, 20).build());
-            y += 24;
-        }
+        int rowY = this.height / 4 + 80;
         this.addRenderableWidget(
-            Button.builder(Component.translatable("netherlink.friends.title"), button -> this.minecraft.setScreen(new NetherLinkFriendsScreen(this, false)))
-                .bounds(this.width / 2 - 102, y, 204, 20)
+            Button.builder(Component.translatable("netherlink.friends.short"), _ -> this.minecraft.setScreen(new NetherLinkFriendsScreen(this, false)))
+                .bounds(this.width / 2 + 106, rowY, 20, 20)
+                .tooltip(Tooltip.create(Component.translatable("netherlink.friends.tooltip")))
                 .build()
         );
+        if (server != null && server.isPublished()) {
+            this.addRenderableWidget(Button.builder(nli$toggleLabel(server), button -> {
+                boolean open = !ClientP2PController.isFriendsOpen(server);
+                ClientP2PController.setFriendsOpen(this.minecraft, server, open);
+                button.setMessage(nli$toggleLabel(server));
+                button.setTooltip(Tooltip.create(nli$label(server)));
+            }).bounds(this.width / 2 + 130, rowY, 44, 20).tooltip(Tooltip.create(nli$label(server))).build());
+        }
     }
 
-    private static Component label(IntegratedServer server) {
+    @Unique
+    private static Component nli$label(IntegratedServer server) {
         return Component.translatable(ClientP2PController.isFriendsOpen(server) ? "netherlink.lan.friends.close" : "netherlink.lan.friends.open");
+    }
+
+    @Unique
+    private static Component nli$toggleLabel(IntegratedServer server) {
+        return ClientP2PController.isFriendsOpen(server) ? CommonComponents.OPTION_ON : CommonComponents.OPTION_OFF;
     }
 }
