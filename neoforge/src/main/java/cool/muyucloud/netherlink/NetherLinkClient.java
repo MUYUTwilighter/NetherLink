ee4d4a91e3f62af559679f5f316b507474047aa1
MUYU_Twilighter <1484605372@qq.com>
2026-06-21 14:21:17 +0800

    fix dup mod annotation

--- neoforge/src/main/java/cool/muyucloud/netherlink/NetherLinkClient.java
+++ neoforge/src/main/java/cool/muyucloud/netherlink/NetherLinkClient.java
@@ -4,17 +4,17 @@ import cool.muyucloud.netherlink.teacon.CommonReg;
 import cool.muyucloud.netherlink.teacon.client.renderer.IntroCardHangingSignRenderer;
 import cool.muyucloud.netherlink.teacon.client.renderer.IntroCardStandingSignRenderer;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
-@Mod(value = NliConstants.MOD_ID, dist = Dist.CLIENT)
-public class NetherLinkClient {
-    public NetherLinkClient(IEventBus eventBus) {
-        eventBus.addListener(this::onRegisterRenderers);
+@EventBusSubscriber(modid = NliConstants.MOD_ID, value = Dist.CLIENT)
+public final class NetherLinkClient {
+    private NetherLinkClient() {
     }
 
-    private void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
+    @SubscribeEvent
+    private static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(
             CommonReg.SIGN_BLOCK_ENTITY.get(),
             IntroCardStandingSignRenderer::new
