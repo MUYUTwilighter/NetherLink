3bfd907c0d9055cf956fd8d02d35422ae99ab0ff
MUYU_Twilighter <1484605372@qq.com>
2026-07-06 15:44:51 +0800

    use netherlink icon

---
+++ common/src/main/java/cool/muyucloud/netherlink/client/NetherLinkIconButton.java
@@ -0,0 +1,32 @@
+package cool.muyucloud.netherlink.client;
+
+import cool.muyucloud.netherlink.NliConstants;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+public final class NetherLinkIconButton extends Button {
+    private static final int BUTTON_SIZE = 20;
+    private static final int ICON_SIZE = 16;
+    private static final int TEXTURE_SIZE = 64;
+    private static final ResourceLocation ICON = ResourceLocation.tryBuild(NliConstants.MOD_ID, "textures/gui/icon.png");
+
+    public NetherLinkIconButton(int x, int y, OnPress onPress) {
+        super(x, y, BUTTON_SIZE, BUTTON_SIZE, Component.empty(), onPress, supplier -> Component.translatable("netherlink.friends.title"));
+        this.setTooltip(Tooltip.create(Component.translatable("netherlink.friends.tooltip")));
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        super.renderWidget(graphics, mouseX, mouseY, partialTick);
+        int iconSize = Math.min(ICON_SIZE, Math.min(this.getWidth() - 4, this.getHeight() - 4));
+        int x = this.getX() + (this.getWidth() - iconSize) / 2;
+        int y = this.getY() + (this.getHeight() - iconSize) / 2;
+        float tint = this.active ? 1.0F : 0.55F;
+        graphics.setColor(tint, tint, tint, this.alpha);
+        graphics.blit(ICON, x, y, iconSize, iconSize, 0.0F, 0.0F, TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE);
+        graphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+}
