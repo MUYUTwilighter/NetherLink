8579942893da7b273f56930a9b136a0230a0a116
MUYU_Twilighter <1484605372@qq.com>
2026-07-21 00:06:48 +0800

    remove shadows

--- common/src/main/java/cool/muyucloud/netherlink/mixin/CommandSourceStackMixin.java
+++ common/src/main/java/cool/muyucloud/netherlink/mixin/CommandSourceStackMixin.java
@@ -4,25 +4,21 @@ import cool.muyucloud.netherlink.access.Messenger;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.function.Supplier;
 
 @Mixin(CommandSourceStack.class)
 public abstract class CommandSourceStackMixin implements Messenger {
-    @Shadow
-    public abstract void sendSystemMessage(Component message);
+    @Invoker("sendSystemMessage")
+    public abstract void nli$sendSystemMessage(Component message);
 
-    @Shadow
-    public abstract boolean hasPermission(int permissionLevel);
+    @Override
+    @Invoker("hasPermission")
+    public abstract boolean nli$hasPermission(int permissionLevel);
 
     @Override
     public void nli$sendMessage(Supplier<Component> msg) {
-        this.sendSystemMessage(msg.get());
-    }
-
-    @Override
-    public boolean nli$hasPermission(int permissionLevel) {
-        return this.hasPermission(permissionLevel);
+        this.nli$sendSystemMessage(msg.get());
     }
 }
